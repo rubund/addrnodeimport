f66b1c83fd3716fb4c41aa25d605f849ddaa9ed5
Ruben Undheim <ruben.undheim@gmail.com>
2014-09-23 17:27:03 +0200

    getedges working

--- src/getedges.c
+++ src/getedges.c
@@ -3,21 +3,52 @@
 #include <libxml/tree.h>
 
 
+char verbose = 0;
+
 void iterate_and_get_elements(xmlNode * a_node){
 	xmlNode *cur_node = NULL;
 	xmlAttr *attribute;
 	xmlChar *text;
 
+	double minLatitude = 200;
+	double maxLatitude = -200;
+	double minLongitude = 200;
+	double maxLongitude = -200;
+	double latitude;
+	double longitude;
+
 	for(cur_node = a_node->children; cur_node; cur_node = cur_node->next){
 		if(cur_node->type == XML_ELEMENT_NODE) {
 			attribute = cur_node->properties;
-			printf("node type: Element, name: %s\n", cur_node->name);
+			if(verbose)
+				printf("node type: Element, name: %s\n", cur_node->name);
 			text = xmlGetProp(cur_node,"lat");
-			printf("  latitude: %s\n", text);
+			latitude = atof(text);
+			if(verbose)
+				printf("  latitude: %s\n", text);
 			text = xmlGetProp(cur_node,"lon");
-			printf("  longitude: %s\n", text);
-		}
+			longitude = atof(text);
+			if(verbose)
+				printf("  longitude: %s\n", text);
+			if(latitude > maxLatitude) maxLatitude = latitude;
+			if(latitude < minLatitude) minLatitude = latitude;
+			if(longitude < minLongitude) minLongitude = longitude;
+			if(longitude > maxLongitude) maxLongitude = longitude;
+		} 
 	}
+
+	if(verbose){
+		printf("The maximum latitude is: %8.8f\n",maxLatitude);
+		printf("The minimum latitude is: %8.8f\n",minLatitude);
+		printf("The maximum longitude is: %8.8f\n",maxLongitude);
+		printf("The minimum longitude is: %8.8f\n",minLongitude);
+	}
+	printf("(");
+	printf("%8.7f,",minLatitude);
+	printf("%8.7f,",minLongitude);
+	printf("%8.7f,",maxLatitude);
+	printf("%8.7f",maxLongitude);
+	printf(")\n");
 
 }
 
@@ -26,8 +57,10 @@ int main(int argc, char **argv){
 	xmlDoc *doc = NULL;
 	xmlNode *root_element = NULL;
 	
-	if(argc != 2)
+	if(argc != 2){
+		fprintf(stderr, "Usage: %s <osm-file>\n",argv[0]);
 		return(-1);
+	}
 
 	LIBXML_TEST_VERSION
 
