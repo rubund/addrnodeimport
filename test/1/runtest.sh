e7c636fabada27684edcb097af857819b928b1bb
Ruben Undheim <ruben.undheim@gmail.com>
2014-10-03 18:06:43 +0200

    Added more to test case

--- test/1/runtest.sh
+++ test/1/runtest.sh
@@ -2,6 +2,8 @@
 
 ../../src/getmissingandreport -s -c corrections.xml -t veiveg.xml.out -w existingways.osm existingnodes.osm newnodes.osm | tee response1-1.txt.out
 ../../src/getmissingandreport -s -c corrections.xml -t veiveg.xml.out existingnodes.osm newnodes.osm | tee response1-2.txt.out
+../../src/getmissingandreport -s -t veiveg.xml.out existingnodes.osm newnodes.osm | tee response1-3.txt.out
+../../src/getmissingandreport -s -t veiveg.xml.out -w existingways.osm existingnodes.osm newnodes.osm | tee response1-4.txt.out
 
 diff response1-1.txt response1-1.txt.out
 if [ $? -ne 0 ]
@@ -9,6 +11,16 @@ then echo "Failed test"
 return -1
 fi
 diff response1-2.txt response1-2.txt.out
+if [ $? -ne 0 ]
+then echo "Failed test"
+return -1
+fi
+diff response1-3.txt response1-3.txt.out
+if [ $? -ne 0 ]
+then echo "Failed test"
+return -1
+fi
+diff response1-4.txt response1-4.txt.out
 if [ $? -ne 0 ]
 then echo "Failed test"
 return -1
