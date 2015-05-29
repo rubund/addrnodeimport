07c365180f2a65de52ea0f0dba7ceb12ab7fd177
Ruben Undheim <ruben.undheim@gmail.com>
2015-05-29 20:11:33 +0200

    Finding all matches

--- python/find_splitted_housenumbers.py
+++ python/find_splitted_housenumbers.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import xml.dom.minidom
 import sqlite3
 
 if len(sys.argv) != 3:
-	print "Usage "+sys.argv[0]+" <notmatched.osm> <newnodes.osm>"
+	print ("Usage "+sys.argv[0]+" <notmatched.osm> <newnodes.osm>")
 	sys.exit(-1)
 
 
@@ -35,7 +35,7 @@ def fill_into_sql(cur, nodes, sqltablename):
 			elif attr == "addr:city":
 				addr_city = tag.getAttribute("v")
 		query = "insert into "+str(sqltablename)+" (id,addr_housenumber, addr_street, addr_postcode, addr_city) values ('"+str(idcnt)+"','"+addr_housenumber+"','"+addr_street+"','"+addr_postcode+"','"+addr_city+"');"
-		print query
+		#print query
 		cur.execute(query)
 		idcnt = idcnt + 1
 
@@ -46,12 +46,23 @@ notmatched_nodes = notmatched_top.getElementsByTagName("node")
 fill_into_sql(cur, notmatched_nodes, "notmatched")
 conn.commit()
 
-newnodes = xml.dom.minidom.parse(sys.argv[1])
+newnodes = xml.dom.minidom.parse(sys.argv[2])
 newnodes_top = newnodes.documentElement
 newnodes_nodes = newnodes_top.getElementsByTagName("node")
 fill_into_sql(cur, newnodes_nodes, "newnodes")
 conn.commit()
-		
+
+cur2 = conn.cursor()
+cur.execute("select * from notmatched")	
+for row in cur.fetchall():
+	addr_street = row[2]
+	addr_housenumber = row[1]
+	cur2.execute("select * from newnodes where addr_street = '"+addr_street+"' and (addr_housenumber like '"+addr_housenumber+"a' or addr_housenumber like '"+addr_housenumber+"b' or addr_housenumber like '"+addr_housenumber+"c' or addr_housenumber like '"+addr_housenumber+"d' or addr_housenumber like '"+addr_housenumber+"e' or addr_housenumber like '"+addr_housenumber+"f' or addr_housenumber like '"+addr_housenumber+"g' or addr_housenumber like '"+addr_housenumber+"h' or addr_housenumber like '"+addr_housenumber+"i')")
+	res = cur2.fetchall()
+	if len(res) != 0:
+		print (addr_street+" "+addr_housenumber)
+		for rowres in res:
+			print (rowres)
 
 
 conn.close()
