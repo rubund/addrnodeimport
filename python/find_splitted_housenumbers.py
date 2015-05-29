12e2e2ffb18234fc70c13a1453ad9fb00c045b77
Ruben Undheim <ruben.undheim@gmail.com>
2015-05-29 21:02:01 +0200

    Also working special symbols in street names

--- python/find_splitted_housenumbers.py
+++ python/find_splitted_housenumbers.py
@@ -34,9 +34,9 @@ def fill_into_sql(cur, nodes, sqltablename):
 				addr_postcode = tag.getAttribute("v")
 			elif attr == "addr:city":
 				addr_city = tag.getAttribute("v")
-		query = "insert into "+str(sqltablename)+" (id,addr_housenumber, addr_street, addr_postcode, addr_city) values ('"+str(idcnt)+"','"+addr_housenumber+"','"+addr_street+"','"+addr_postcode+"','"+addr_city+"');"
-		#print query
-		cur.execute(query)
+		query = "insert into "+str(sqltablename)+" (id,addr_housenumber, addr_street, addr_postcode, addr_city) values ('"+str(idcnt)+"',?,?,?,?);"
+		values = (addr_housenumber,addr_street,addr_postcode,addr_city)
+		cur.execute(query,values)
 		idcnt = idcnt + 1
 
 
@@ -61,7 +61,9 @@ cur.execute("select * from notmatched")
 for row in cur.fetchall():
 	addr_street = row[2]
 	addr_housenumber = row[1]
-	cur2.execute("select * from newnodes where addr_street = '"+addr_street+"' and (addr_housenumber like '"+addr_housenumber+"a' or addr_housenumber like '"+addr_housenumber+"b' or addr_housenumber like '"+addr_housenumber+"c' or addr_housenumber like '"+addr_housenumber+"d' or addr_housenumber like '"+addr_housenumber+"e' or addr_housenumber like '"+addr_housenumber+"f' or addr_housenumber like '"+addr_housenumber+"g' or addr_housenumber like '"+addr_housenumber+"h' or addr_housenumber like '"+addr_housenumber+"i') order by addr_street, addr_housenumber")
+	addr_postcode = row[3]
+	addr_city = row[4]
+	cur2.execute("select * from newnodes where addr_street = ? and addr_postcode = ? and addr_city = ? and (addr_housenumber like '"+addr_housenumber+"a' or addr_housenumber like '"+addr_housenumber+"b' or addr_housenumber like '"+addr_housenumber+"c' or addr_housenumber like '"+addr_housenumber+"d' or addr_housenumber like '"+addr_housenumber+"e' or addr_housenumber like '"+addr_housenumber+"f' or addr_housenumber like '"+addr_housenumber+"g' or addr_housenumber like '"+addr_housenumber+"h' or addr_housenumber like '"+addr_housenumber+"i') order by addr_street, addr_housenumber",(addr_street,addr_postcode,addr_city))
 	res = cur2.fetchall()
 	if len(res) != 0:
 		first = 1
