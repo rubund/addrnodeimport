c6ba923c908f1635cfcf2905c69875cedef5d9e0
Ruben Undheim <ruben.undheim@gmail.com>
2017-05-08 19:12:02 +0200

    Fixed little bug

--- src/universal.c
+++ src/universal.c
@@ -951,7 +951,7 @@ void add_new_nodes_which_are_not_close_to_any_old_ones(sqlite3 *db, double meter
         longitude = sqlite3_column_double(stmt,6);
         lonmargin = meter_to_longitude(meter_margin, latitude);
         //printf("latitude: %f\n", latitude);
-        querybuffer = sqlite3_mprintf("select id, addr_street, addr_housenumber, addr_city, addr_postcode from existing where foundindataset = 0 and latitude > '%f' and latitude < '%f' and longitude > '%f' and longitude < '%f';", latitude - latmargin, latitude + latmargin, longitude - lonmargin, longitude + lonmargin);
+        querybuffer = sqlite3_mprintf("select id, addr_street, addr_housenumber, addr_city, addr_postcode from existing where foundindataset = 0 and latitude > '%f' and latitude < '%f' and longitude > '%f' and longitude < '%f' and ((tag_number <= 4) or (tag_number <= 5 and building = 1));", latitude - latmargin, latitude + latmargin, longitude - lonmargin, longitude + lonmargin);
         //printf("%s\n", querybuffer);
         ret = sqlite3_prepare_v2(db,querybuffer,-1,&stmt2,0);
         sqlite3_free(querybuffer);
