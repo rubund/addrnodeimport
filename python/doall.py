b29d9bbd66bc33db8ed4f5803b3e12ee287e1c22
Ruben Undheim <ruben.undheim@gmail.com>
2017-05-21 11:55:30 +0200

    Easier switching between update modes

--- python/doall.py
+++ python/doall.py
@@ -10,12 +10,20 @@ db = mysql.connector.connect(host="localhost",user=mypasswords.sqldbuser,passwor
 cursor = db.cursor()
 cursor.execute("set names utf8")
 
-cursor.execute("select updated from municipalities where muni_id="+str(sys.argv[1])+" and updated > date_sub(now(),INTERVAL 2 DAY)")
+# Mode==0: Downloads existing data directly (network intensive, but fresh data)
+# Mode==1: Downloads all of Norway (1-2 days old, then extracts)
+mode = 1
+
+if mode == 0:
+	cursor.execute("select updated from municipalities where muni_id="+str(sys.argv[1])+" and updated > date_sub(now(),INTERVAL 0 DAY)")
+else:
+	cursor.execute("select updated from municipalities where muni_id="+str(sys.argv[1])+" and updated > date_sub(now(),INTERVAL 2 DAY)")
+
 rows = cursor.fetchall()
 db.close()
 if len(rows) == 0:
-	os.system("/usr/bin/addrnodeimport /var/cache/addrnodeimport/Vegdata_Norge_Adresser_UTM33_SOSI.zip "+sys.argv[1]+" /var/cache/addrnodeimport 1")
-	os.system("/usr/lib/addrnodeimport/python/updatetime.py "+sys.argv[1]+"")
+	os.system("/usr/bin/addrnodeimport /var/cache/addrnodeimport/Vegdata_Norge_Adresser_UTM33_SOSI.zip "+sys.argv[1]+" /var/cache/addrnodeimport "+str(mode))
+	os.system("/usr/lib/addrnodeimport/python/updatetime.py "+sys.argv[1]+" "+str(mode)+"")
 	os.system("/usr/lib/addrnodeimport/python/uploadmunip_new.py "+sys.argv[1]+"")
 else:
 	print ("Not doing anything for "+sys.argv[1])
--- python/updatetime.py
+++ python/updatetime.py
@@ -5,18 +5,26 @@ import sys
 import mypasswords
 
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 2 and len(sys.argv) != 3:
 	print ("Not correct argument")
 	sys.exit(-1)
 
 muninumber = int(sys.argv[1])
+
+if len(sys.argv) == 3:
+	mode = int(sys.argv[2])
+else:
+	mode = 1
 
 
 db = mysql.connector.connect(host="localhost",user=mypasswords.sqldbuser,password=mypasswords.sql,database=mypasswords.sqldbname)
 ecursor = db.cursor()
 ecursor.execute("set names utf8")
 
-updatestring = "date_sub(now(),INTERVAL 2 DAY)"
+if mode == 0:
+	updatestring = "date_sub(now(),INTERVAL 0 DAY)"
+else:
+	updatestring = "date_sub(now(),INTERVAL 2 DAY)"
 #updatestring = "now()"
 
 uquery = "update municipalities set updated="+updatestring+" where muni_id=\""+str(muninumber)+"\";"
