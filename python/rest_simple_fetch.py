6949532c74562fff7756fa047fbb0afe8b97e460
Ruben Undheim <ruben.undheim@gmail.com>
2018-05-27 16:00:30 +0200

    Started rest api test

---
+++ python/rest_simple_fetch.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+import os
+
+os.system("wget -O test.htm \"http://ws.geonorge.no/AdresseWS/adresse/boundingbox?nordLL=60.015&austLL=10.030&nordUR=60.315&austUR=10.430\"")
