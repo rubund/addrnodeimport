2ae9293ca6b40403138ed11a54049be928654211
Ruben Undheim <ruben.undheim@gmail.com>
2018-05-27 16:00:30 +0200

    Split out variables

--- python/rest_simple_fetch.py
+++ python/rest_simple_fetch.py
@@ -3,4 +3,12 @@
 
 import os
 
-os.system("wget -O test.htm \"http://ws.geonorge.no/AdresseWS/adresse/boundingbox?nordLL=60.015&austLL=10.030&nordUR=60.315&austUR=10.430\"")
+nordLL=63
+nordUR=64
+austLL=10.2
+austUR=11
+
+antPerSide=20
+side=0
+
+os.system("wget -O test.htm \"http://ws.geonorge.no/AdresseWS/adresse/boundingbox?nordLL=%.3f&austLL=%.3f&nordUR=%.3f&austUR=%.3f&antPerSide=%d&side=%d\"" % (nordLL, austLL, nordUR, austUR, antPerSide, side))
